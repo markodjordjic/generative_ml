edc4247d24c64ffaeb6da89a7d418d1e66795636
Marko Djordjic <marko.djordjic@outlook.com>
2024-07-20 14:50:12 +0200

    Sucessful response.

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from miscallaneous.api_call import Caller
 
 if __name__ == '__main__':
 
-    prompt = 'How is the weather today in Belgrade?'
+    prompt = 'Generate a list of top 10 movies of all time'
 
-    caller = Caller(prompt=prompt)
+    caller = Caller(prompt=prompt, max_tokens=64)
     caller.make_call()
     response = caller.get_response()
+    print(response)
 
     sys.exit(0)
--- miscallaneous/api_call.py
+++ miscallaneous/api_call.py
@@ -1,28 +1,45 @@
+from pathlib import Path
+from dotenv import dotenv_values
 import openai
+
+env_file = Path(r'./.env')
+environment = dotenv_values(env_file)
 
 class Caller:
 
+    openai.api_key = environment['OPEN_API_KEY']
+
     def __init__(self,
-                 model: str = 'text-davinci-003',
+                 model: str = 'gpt-3.5-turbo-instruct',
+                 max_tokens: int = 32,
                  prompt: str = None):
         self.model = model
         self.prompt = prompt
-        self.response = None
+        self.max_tokens = max_tokens
+        self.raw_response = None
+        self.post_processed_response = None
         
+    def _extract_response(self):
+
+        assert self.raw_response is not None, 'No response.'
+
+        self.post_processed_response = self.raw_response['choices'][0]['text']
+    
     def make_call(self):
 
         assert self.prompt is not None, 'No prompt.'
 
-        self.response = openai.completions.create(
+        self.raw_response = openai.completions.create(
             model=self.model,
+            max_tokens=self.max_tokens,
             prompt=self.prompt
-        )
+        ).to_dict()
 
     def get_response(self):
 
-        assert self.response is not None, 'No response.'
+        self._extract_response()
 
-        return self.response
+        return self.post_processed_response
 
 
         
