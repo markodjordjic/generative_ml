a05f047886921922529dc5ad1ee6e424341ab911
Marko Djordjic <marko.djordjic@outlook.com>
2024-07-24 18:33:28 +0200

    feat(proposal_manager): Manager of the proposals finished.

--- cocktail_sort.py
+++ cocktail_sort.py
@@ -1,11 +1,12 @@
 # Python program for implementation of Cocktail Sort
 
+
 def cocktailSort(a):
 	n = len(a)
 	swapped = True
 	start = 0
 	end = n-1
-	while (swapped==True):
+	while (swapped == True):
 
 		# reset the swapped flag on entering the loop,
 		# because it might be true from a previous
@@ -14,13 +15,13 @@ def cocktailSort(a):
 
 		# loop from left to right same as the bubble
 		# sort
-		for i in range (start, end):
-			if (a[i] > a[i+1]) :
-				a[i], a[i+1]= a[i+1], a[i]
-				swapped=True
+		for i in range(start, end):
+			if (a[i] > a[i + 1]):
+				a[i], a[i + 1] = a[i + 1], a[i]
+				swapped = True
 
 		# if nothing moved, then array is sorted.
-		if (swapped==False):
+		if (swapped == False):
 			break
 
 		# otherwise, reset the swapped flag so that it
@@ -33,19 +34,20 @@ def cocktailSort(a):
 
 		# from right to left, doing the same
 		# comparison as in the previous stage
-		for i in range(end-1, start-1,-1):
-			if (a[i] > a[i+1]):
-				a[i], a[i+1] = a[i+1], a[i]
+		for i in range(end-1, start-1, -1):
+			if (a[i] > a[i + 1]):
+				a[i], a[i + 1] = a[i + 1], a[i]
 				swapped = True
 
 		# increase the starting point, because
 		# the last stage would have moved the next
 		# smallest number to its rightful spot.
-		start = start+1
+		start = start + 1
 
-# Driver code to test above
+
+# Driver code 
 a = [5, 1, 4, 2, 8, 0, 2]
 cocktailSort(a)
 print("Sorted array is:")
 for i in range(len(a)):
-	print ("%d" %a[i]),
+	print("% d" % a[i])
--- code_reviewer.py
+++ code_reviewer.py
@@ -1,10 +1,11 @@
-from operations.code_revirewing import CodeReviewChatBot
+from operations.reviewing import CodeReviewChatBot
 from tenacity import (
     retry, 
     retry_if_exception_type, 
     stop_after_attempt, 
     wait_random_exponential
 )
+
 
 if __name__ == '__main__':
 
@@ -13,23 +14,23 @@ if __name__ == '__main__':
     with open(python_file, 'r') as file:
         content = file.read()
 
-    code_reviewer = CodeReviewChatBot(personality=None,
+    code_reviewer = CodeReviewChatBot(personality='helpful',
                                       program_text=content)
     code_reviewer.start_review()
     review = code_reviewer.get_history()
 
-    start_point = generate_base_messages(
-        content,
-        ignore_list=ignore_list,
-        accept_list=accept_list
-    )
+    # start_point = generate_base_messages(
+    #     content,
+    #     ignore_list=ignore_list,
+    #     accept_list=accept_list
+    # )
 
-    while True:
-        code_review = code_review(start_point, content)
+    # while True:
+    #     code_review = code_review(start_point, content)
 
-        if len(code_review.suggested_changes.changes) == 0:
-            print('No changes were required.')
+    #     if len(code_review.suggested_changes.changes) == 0:
+    #         print('No changes were required.')
         
-        else:
-            proposed_change = code_review.suggested_changes.changes
-            explanations = code_review.suggested_changes.messages
+    #     else:
+    #         proposed_change = code_review.suggested_changes.changes
+    #         explanations = code_review.suggested_changes.messages
