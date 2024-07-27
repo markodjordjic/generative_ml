06ce47d39868361d17db33a0f1f6cf43090c1db4
Marko Djordjic <marko.djordjic@outlook.com>
2024-07-27 17:51:24 +0200

    feat(similarity_measurement): Measurement of similarity for top 5 items.

--- embeddings.py
+++ embeddings.py
@@ -1,5 +1,10 @@
 import pandas as pd
 from operations.calling import Embeddings
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import euclidean_distances
+import numpy as np
+
 
 if __name__ == '__main__':
 
@@ -19,3 +24,26 @@ if __name__ == '__main__':
 
     len(film_summary_embeddings)
     film_summary_tabe = pd.DataFrame.from_records(film_summary_embeddings)
+
+    latent_feature_analysis = PCA()
+    latent_feature_analysis.fit(film_summary_tabe)
+    dimensionality_rediction = latent_feature_analysis.fit_transform(X=film_summary_tabe)
+    plt.scatter(dimensionality_rediction[:, 0], dimensionality_rediction[:, 1])
+    plt.show()
+
+    distances = euclidean_distances(dimensionality_rediction)
+    zero_mask = distances == 0.0
+    distances[zero_mask] = np.nan    
+    mask = data.loc[:, 'film_name'] == 'The Batman'
+    index = data.index[mask].to_list()[0]
+    distances_per_film = distances[index, :]
+    top_k = 5
+    k = []
+    for top_k_index in range(0, top_k):
+        top_k_distances = np.nanargmin(distances_per_film).tolist()
+        k.extend([top_k_distances])
+        distances_per_film[top_k_distances] = np.nan
+    
+    for most_similar in k:
+        print(data.iloc[most_similar]['film_name'])
+
