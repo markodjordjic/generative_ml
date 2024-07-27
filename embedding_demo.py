118787f646f44b44aa1e2a361795a81c3912480d
Marko Djordjic <marko.djordjic@outlook.com>
2024-07-27 22:58:21 +0200

    docs(notebook): Rendering of the notebook to html.

--- embedding_demo.py
+++ embedding_demo.py
@@ -1,22 +1,22 @@
 # %% [markdown]
 # # Demonstration of Embeddings for Similarity Measure
 # Description of the demo.
-# %%[markdown]
+# %% [markdown]
 # Firstly, let us make the necessary imports.
 # %%
 import pandas as pd
 from operations.calling import Embeddings
 from embeddings.embeddings import LatentFeatureAnalysis, SimilarityMeasurement
-# %%[markdown]
+# %% [markdown]
 # # Import the data
-# The data consits of movie plots scraped from Wikipedia. In this
+# The data consists of movie plots scraped from Wikipedia. In this
 # case move plots are movies belonging from the adventure genre,
 # starting from 1980s up to the 2020s.
 # %%
 raw_data = pd.read_csv(
     filepath_or_buffer=r"D:\Projects\025_scenario_writing\other\output\screenplays_adventure_films_1980_2020_prepared.csv"
 )
-# %%[markdown]
+# %% [markdown]
 # It is fairly easy to generate embeddings.
 # %%
 film_summary_embeddings = []
@@ -27,7 +27,7 @@ for film_name, film_summary in zip(raw_data.loc[:, 'film_name'], raw_data.loc[:,
     film_summary_embeddings.extend([embedding.get_embeddings()])
 
 film_data = pd.DataFrame.from_records(film_summary_embeddings)
-# %%[markdown]
+# %% [markdown]
 # Once that embeddings are available, it is possible to perform some of
 # the latent feature analyses. This will (a) preserve the most of the
 # variance in the data, while at the same time providing us (b) to
@@ -41,7 +41,7 @@ latent_feature_analyses = LatentFeatureAnalysis(
     method='PCA'
 )
 latent_feature_analyses.execute_analysis()
-# %%[markdown]
+# %% [markdown]
 # Let us now empirically evaluate the quality of reduction. This can be
 # done by selecting a reference movie, and finding the most similar 
 # movies by their plots.
@@ -49,7 +49,7 @@ latent_feature_analyses.execute_analysis()
 reference_movie = 'The Batman'
 mask = raw_data.loc[:, 'film_name'] == reference_movie
 reference_index = raw_data.index[mask].to_list()[0]
-# %%[markdown]
+# %% [markdown]
 # Now that index of the reference movie is retrieved, it is possible to
 # utilize it in the measurement of similarity and identification of the
 # most similar movie plots. Count of most similar movie plots is set to
@@ -63,7 +63,7 @@ similarity_measurement = SimilarityMeasurement(
 )
 similarity_measurement.compute_distances()
 top_most_similar = similarity_measurement.get_top_k()
-# %%[markdown]
+# %% [markdown]
 # Let us discover most similar movies, to our reference movie, according
 # embeddings and LFA performed.
 # %%
