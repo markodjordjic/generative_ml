980cb325a0f6367c1b80776512f35d091e0fff3c
Marko Djordjic <marko.djordjic@outlook.com>
2024-07-27 16:15:43 +0200

    feat(embedding_factilitation): Introduction of methods for large scale embedding.

--- embeddings.py
+++ embeddings.py
@@ -4,11 +4,18 @@ from operations.calling import Embeddings
 if __name__ == '__main__':
 
     data = pd.read_csv(
-        filepath_or_buffer='"D:\Projects\025_scenario_writing\other\output\screenplays_adventure_films_1980_2020_prepared.csv"'
+        filepath_or_buffer=r"D:\Projects\025_scenario_writing\other\output\screenplays_adventure_films_1980_2020_prepared.csv"
     )
 
 
-    embedding = Embeddings(
-        raw_text=None
-    )
-    embedding.generate_embeddings()
+    film_summary_embeddings = []
+    for film_name, film_summary in zip(data.loc[:, 'film_name'], data.loc[:, 'content']):
+        print(f'Making embedding for {film_name}')
+        embedding = Embeddings(raw_text=film_summary)
+        embedding.generate_embeddings()
+        film_summary_embeddings.extend(
+            [embedding.get_embeddings()]
+        )
+
+    len(film_summary_embeddings)
+    film_summary_tabe = pd.DataFrame.from_records(film_summary_embeddings)
